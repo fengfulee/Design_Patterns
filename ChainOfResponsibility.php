9df44e58b600d1b167c7f88f530b50eab923b24b
= <=>
2014-03-11 16:36:28 +0800

    责任链模式和迭代器模式

--- ChainOfResponsibility.php
+++ ChainOfResponsibility.php
@@ -1,3 +1,60 @@
 <?PHP
+/*
+为解除请求的发送者和接收者之间的耦合,而使用多个对象都用机会处理这个请求,将这些对象连成一条链,并沿着这条链传递该请求,直到有一个对象处理它
 
+*/
+
+	abstract class Handler{
+		protected $_handler = null;
+		//设置继承者..
+		public function setSuccessor($handler){
+			$this->_handler = $handler;
+		}
+	}
+
+	class CheckZeroHandler extends Handler{	
+		public function handleRequest($request){
+			if($request == 0){
+				echo "0 is ZERO\n";
+			}else{
+				$this->_handler->handleRequest($request);
+			}
+		}
+		
+	}
+
+	class CheckEvenHandler extends Handler{
+		public function handleRequest($request){
+			if($request%2==0){
+				echo "$request is EVEN\n";
+			}else{
+				$this->_handler -> handleRequest($request);
+			}
+		}
+	}
+
+	class CheckOddHandler extends Handler{
+		public function handleRequest($request){	
+			if($request%2!=0){
+				echo "$request is ODD\n";
+			}else{
+				$this->_handler ->handleRequest($request);
+			}
+		}
+	}
+
+
+
+?>
+<?PHP
+	$checkzero = new CheckZeroHandler();
+	$checkeven = new CheckEvenHandler();
+	$checkodd = new CheckOddHandler();
+
+	$checkzero->setSuccessor($checkeven);
+	$checkeven->setSuccessor($checkodd);
+	$arr = array(3,4,5,6,0);
+	foreach($arr as $v){
+		$checkzero->handleRequest($v);
+	}
 ?>
--- iterator_pattern.php
+++ iterator_pattern.php
@@ -1,2 +1,46 @@
 <?PHP
+//该类实现了迭代器模式....
+//迭代器的使用场景..
+//1 访问一个聚合对象的内容而无需暴露它的内部表示
+//2 支持对聚合对象的多种遍历.
+//3 为遍历不同的聚合结构提供统一的接口...
+class MyIterator implements Iterator{
+	//定义一个数组..
+	private $arr = array();
+	
+	public function __construct(array $arr){
+		$this->arr = $arr;
+	}
+
+	//实现Iterator接口需要实现5个方法..
+	//rewind,current,key,valid,next
+
+	public function rewind(){
+		reset($this->arr);
+	}
+
+	public function current(){
+		return current($this->arr);
+	}
+
+	public function key(){
+		return key($this->arr);
+	}
+
+	public function next(){
+		return next($this->arr);
+	}
+
+	public function valid(){	
+		return $this->current()!==false;	//如果有内容则返回 true,否则,false;
+	}
+}
+?>
+
+<?PHP
+	$arr = array(1,2,3,4,5);
+	$iterator = new MyIterator($arr);
+	foreach($iterator as $value){
+		echo $value,"\n";
+	}
 ?>
