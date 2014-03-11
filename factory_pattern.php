25d15ab49def6cdbf6d0f5cd426e4f3e1cb5b97b
= <=>
2014-03-11 17:11:15 +0800

    添加了工厂模式的实现

--- factory_pattern.php
+++ factory_pattern.php
@@ -1,3 +1,61 @@
 <?PHP
+//工厂模式,相关类的实例化交给这个类来处理..
 
+	//定义一个human的接口..
+	interface Human{
+		public function getName();
+		public function setName($name);
+	}
+
+	class Teacher implements Human{
+		private $name;
+		public function setName($name) {
+			$this-> name = $name;
+		}
+		public function getName(){
+			$_name = $this->name;
+			return $_name;
+		}
+		public function __construct($name){
+			$this->name = $name;
+		}
+		public function teach(){
+			echo "I am a teacher,My name is {$this->name},I love teaching!\n";
+		}
+	}
+
+	class Student implements Human{
+		private $name;
+		public function setName($name){
+			$this->name = $name;
+		}
+		public function __construct($name){
+			$this->name = $name;
+		}
+		public function getName(){
+			return $this->name;
+		}
+
+		public function study(){	
+			echo "I am a student,My name is {$this->name},I love studying!\n";	
+		}
+	}
+
+	class Factory{
+		
+		public static function  _factory($name,$args=null){
+			if(class_exists($name)){
+				return new $name($args);
+			}else{
+				return null;
+			}
+		}
+	}
+?>
+
+<?PHP
+	$teacher = Factory::_factory('Teacher','fengfulee');
+	$student = Factory::_factory('Student','lichao');
+	$teacher->teach();
+	$student->study();
 ?>
