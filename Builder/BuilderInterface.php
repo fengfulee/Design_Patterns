76281978db7fcc8124eaf52fe35cfdc219699f6b
fengfulee <fengfulee@163.com>
2014-03-30 22:40:18 +0800

    Update BuilderInterface.php

--- Builder/BuilderInterface.php
+++ Builder/BuilderInterface.php
@@ -1,9 +1,10 @@
 <?PHP
 namespace Builder;
   interface BuilderInterface{
-    
+    //首先创建这个对象..
     public function createVehicle();
     
+    //下面是给这个对象添加一些内容...
     public function addWheel();
     
     public function addEngine();
