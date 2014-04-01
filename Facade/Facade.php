7395aca642f55229d753d2c9eb05fc6b175eaada
fengfulee <fengfulee@163.com>
2014-04-01 12:38:37 +0800

    Update Facade.php

--- Facade/Facade.php
+++ Facade/Facade.php
@@ -12,8 +12,9 @@
       
       //这里对外提供两个函数..
       public function turnOn(){
-        //1
+        //1,执行bios程序.
         $this->bios ->execute();
+        //2,加载系统.
         $this->bios -> launch($this->os);
         
       }
@@ -21,8 +22,9 @@
       //
       public function turnOff()
       {
-        
+       //第一步将系统关闭.. 
         $this->os->halt();
+        //第二步将bios关闭.
         $this->bios->powerDown();
       }      
     }//end of clazz...
