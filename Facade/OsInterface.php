a4545b35e025d3d5b1dd6b3afe4fceb147a1697f
fengfulee <fengfulee@163.com>
2014-04-01 12:27:10 +0800

    Create OsInterface.php

---
+++ Facade/OsInterface.php
@@ -0,0 +1,12 @@
+<?PHP
+namespace Facade;
+  interface OsInterface{
+    /*start*/
+    public function start();
+    
+    /*halt*/
+    public function halt();
+    
+    
+  }//end of clazz...
+?>
