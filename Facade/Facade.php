845bc581248867b322e9daa944c69385d2a114ba
fengfulee <fengfulee@163.com>
2014-04-01 12:37:24 +0800

    Update Facade.php

--- Facade/Facade.php
+++ Facade/Facade.php
@@ -1,3 +1,29 @@
 <?PHP
 
+  class Facade{
+      protected $os;
+      protected $bios;
+      
+      public function __construct(BiosInterface $bios,OsInterface $os){
+        $this->os = $os;
+        $this->bios = $bios;
+      }
+      
+      
+      //这里对外提供两个函数..
+      public function turnOn(){
+        //1
+        $this->bios ->execute();
+        $this->bios -> launch($this->os);
+        
+      }
+      
+      //
+      public function turnOff()
+      {
+        
+        $this->os->halt();
+        $this->bios->powerDown();
+      }      
+    }//end of clazz...
 ?>
